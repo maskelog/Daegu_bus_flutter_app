297727128b5d3449c28b9cff9638cb87b600448c
maskelog <blitzmaske@gmail.com>
2025-03-21 17:28:10 +0900

    feat: StationSearchResult 모델 이름 변경 및 로컬 정류장 검색 결과 모델 추가

--- android/app/src/main/kotlin/com/example/daegu_bus_app/StationSearchResult.kt
+++ android/app/src/main/kotlin/com/example/daegu_bus_app/StationSearchResult.kt
@@ -1,7 +1,15 @@
 package com.example.daegu_bus_app
 
-// 정류장 검색 결과 모델
-data class StationSearchResult(
+data class WebStationSearchResult(
     val bsId: String,
     val bsNm: String
+)
+
+data class LocalStationSearchResult(
+    val bsId: String,         // 정류장 고유 ID
+    val bsNm: String,         // 정류장 이름
+    val latitude: Double,     // 위도
+    val longitude: Double,    // 경도
+    var stationId: String? = null, // API 호출에 사용되는 정류장 ID
+    val routeList: String? = null // 정류장에 관한 경로 정보
 )
